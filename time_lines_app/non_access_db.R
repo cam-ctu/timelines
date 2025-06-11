3fde1b6dfb8caa6b8bcd4409c2cc504cb00565b2
shug0131 <sjb277@medschl.cam.ac.uk>
2025-06-11 15:43:01 +0100

    trying out alternatives to access sqllite

--- time_lines_app/non_access_db.R
+++ time_lines_app/non_access_db.R
@@ -1,12 +1,14 @@
 library("DBI")
 library("RSQLite")
-mydb <- dbConnect(RSQLite::SQLite(), "my-db")
+file=paste0(normalizePath(getwd()),"/my-db2")
+# really doesn't like to work on the network drive....
+mydb <- dbConnect(RSQLite::SQLite(), dbname="~/Documents/my-db")
 
-
+mtcars
 dbWriteTable(mydb, "mtcars", mtcars)
 dbWriteTable(mydb, "iris", iris)
 dbListTables(mydb)
-
+dbReadTable(mydb, "mtcars")
 dblsReadOnly(mydb)
 
 dbDisconnect(mydb)
